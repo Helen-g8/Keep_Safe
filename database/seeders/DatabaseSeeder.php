00bfc68d6330afc277fab1ebbcaf2a0087dafe05
Helen-g8 <136003068+Helen-g8@users.noreply.github.com>
2023-06-15 10:39:37 -0600

    seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -21,6 +21,7 @@ class DatabaseSeeder extends Seeder
             UserSeeder::class,
             UserReviewSeeder::class,
             StudentSeeder::class,
+            RentalLocationSeeder::class,
         ]);
     }
 }
--- database/seeders/RentalLocationSeeder.php
+++ database/seeders/RentalLocationSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Models\RentalLocation;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
@@ -12,6 +13,6 @@ class RentalLocationSeeder extends Seeder
      */
     public function run(): void
     {
-    
+    RentalLocation::factory(20)->create();
     }
 }
