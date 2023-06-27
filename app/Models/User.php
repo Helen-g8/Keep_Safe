65488ea9f502b4983ae24824a8ff54637d4da862
alexticas2006 <137197550+alexticas2006@users.noreply.github.com>
2023-06-27 11:05:41 -0600

    Relationships
    Another change on the relationships

--- app/Models/User.php
+++ app/Models/User.php
@@ -52,4 +52,9 @@ class User extends Authenticatable
     {
         return $this->hasMany(UserReview::class);
     }
+
+    public function sex()
+    {
+        return $this->belongsTo(Sex::class);
+    }
 }
