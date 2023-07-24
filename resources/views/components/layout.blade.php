357a122879f617dcd6346cfd42abc544e8219826
eijei-uwu <allison.ruballo2024@poma.superate.org.sv>
2023-07-24 10:54:24 -0600

    Login almost done
    yupi

---
+++ resources/views/components/layout.blade.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=}, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <script src="https://cdn.tailwindcss.com"></script>
+    <title>Document</title>
+</head>
+<body>
+
+    {{ $slot }}
+
+</body>
+</html>
