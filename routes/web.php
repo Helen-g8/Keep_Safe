58882bc9eef18b926be5bf90a21f7c0b5c47e249
Helen-g8 <136003068+Helen-g8@users.noreply.github.com>
2023-07-25 10:55:06 -0600

    singup

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use App\Models\Role;
 use App\Models\Sex;
+use App\Models\User;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -26,20 +27,31 @@ Route::get('/signUp', function () {
     ]);
 });
 
-Route::post('/signUp', function (){
-    $attributes= request()->validate([
+Route::post('/signUp', function () {
+    $attributes = request()->validate([
         'first_name' => 'required|string|max:100',
-        'last_name'=> 'required|string|max:100',
-        'age'=> 'required|integer|max_digits:2|min:15',
-        'DUI' => 'required|integer|max_digits:9|min_digits:9',
-        'phone' => 'required|interger|max-digits:8|min_digits:8'
-        ]);
+        'last_name' => 'required|string|max:100',
+        'age' => 'required|integer|max_digits:2|min:15',
+        'dui' => 'required|integer|max_digits:9|min_digits:9',
+        'phone' => 'required|interger|max-digits:8|min_digits:8',
+        'email' => 'required|email|max:255',
+        'password' => 'required|string|confirmed|max:255',
+        'role_id' => 'required|integer|exists:roles,id',
+        'sex_id' => 'required|integer|exists:sexes,id',
+    ]);
+
+    $user = User::create($attributes);
+
+    Auth::login($user);
+    request()->session()->regenerate();
+
+    return redirect('/home');
 });
 
-Route::get('/login', function(){
+Route::get('/login', function () {
     return view('login');
 });
 
-Route::get('/selectRole',function(){
+Route::get('/selectRole', function () {
     return view('selectRole');
 });
