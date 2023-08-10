9d952a21e88b010ac08627e4ecb9d3e125e52204
eijei-uwu <allison.ruballo2024@poma.superate.org.sv>
2023-08-10 09:49:47 -0600

    rental xd

---
+++ resources/views/components/rentalInformation.blade.php
@@ -0,0 +1,16 @@
+<x-layout>
+
+    <x-input label="name" placeholder="Nombre de tu arrendamiento" name="name" required />
+    <x-input label="ubication" placeholder="Ubicacion" name="ubication" required />
+    <x-input label="" placeholder="Selecciona tu tipo de arrendamiento" name="" required />
+    <x-input label="rollNumber" placeholder="Ingresa el numero de habitacion/es disponibles en tu arrendamiento" name="rollNumber " required />
+    
+
+
+
+
+
+
+
+
+</x-layout>
