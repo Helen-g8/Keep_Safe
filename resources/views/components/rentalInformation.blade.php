a507a5d4205b214f629886cc234d77e88205813f
Hernandez2506 <137920074+Hernandez2506@users.noreply.github.com>
2023-08-10 10:12:33 -0600

    ko

--- resources/views/components/rentalInformation.blade.php
+++ resources/views/components/rentalInformation.blade.php
@@ -1,9 +1,27 @@
 <x-layout>
-
+    <form>
     <x-input label="name" placeholder="Nombre de tu arrendamiento" name="name" required />
     <x-input label="ubication" placeholder="Ubicacion" name="ubication" required />
-    <x-input label="" placeholder="Selecciona tu tipo de arrendamiento" name="" required />
-    <x-input label="rollNumber" placeholder="Ingresa el numero de habitacion/es disponibles en tu arrendamiento" name="rollNumber " required />
+    <select id="tipo_arrendamiento" name="tipo_arrendamiento">
+        <option value="Casa">Casa</option>
+        <option value="Habitación">Habitación</option>
+    </select>
+    <x-input label="number" placeholder="Numero de habitaciones" name="number" required />
+    <x-input label="number" placeholder=" Ingresa el precio de tu arrendamiento" name="number" required />
+    <select id="tiempo_de_arrendamiento_disponible" name="tiempo_de_arrendamiento_disponible">
+        <option value="mensual">Mensual</option>
+        <option value="semestral">Semestral</option>
+        <option value="anual">Anual</option>
+        <option value="personalizado">Personalizado</option>
+</select>
+        <x-input label="text" placeholder="Restrinciones personalizadas" name="text" required />
+        <select id>
+    <x-input label="ubication" placeholder="Ubicacion" name="ubication" required />
+</form>
+    
+
+
+
     
 
 
