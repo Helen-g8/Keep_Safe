6d76b4fe8a9c38962763c9e05f75b64088932b35
alexticas2006 <137197550+alexticas2006@users.noreply.github.com>
2023-08-12 10:48:15 -0600

    cartita moved

--- resources/views/rentalInformation.blade.php
+++ resources/views/rentalInformation.blade.php
@@ -1,43 +1,51 @@
 <x-layout>
-    <form x-data="{ state: '', town: '', district: '' }">
-    <x-input label="name" placeholder="Nombre de tu arrendamiento" name="name" required />
-    <x-input label="ubication" placeholder="Ubicacion" name="ubication" required />
-    <x-input label="number" placeholder="Numero de habitaciones" name="number" required />
-    <x-input label="number" placeholder=" Ingresa el precio de tu arrendamiento" name="number" required />
-        <x-input label="text" placeholder="Restrinciones personalizadas" name="text" required />
-    <x-input label="ubication" placeholder="Ubicacion" name="ubication" required />
+    <div class="mx-6 grid gap-6 md:grid-cols-2">
+        <form x-data="{ state: '', town: '', district: '' }">
+            <x-input label="name" placeholder="Nombre de tu arrendamiento" name="name" required />
+            <x-input label="ubication" placeholder="Ubicacion" name="ubication" required />
+            <x-input label="number" placeholder="Numero de habitaciones" name="number" required />
+            <x-input label="number" placeholder=" Ingresa el precio de tu arrendamiento" name="number" required />
+            <x-input label="text" placeholder="Restrinciones personalizadas" name="text" required />
+            <x-input label="ubication" placeholder="Ubicacion" name="ubication" required />
 
 
-    <div>
-        <label for="sex" class="block mb-2 text-sm font-medium text-white-900">Departamentos</label>
-        <select id="sexSign" x-model="state" class="bg-white-50 border border-gray-300 text-white-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:focus:ring-blue-500 dark:focus:border-blue-500" required>
-            <option value="" selected disabled>Selecciona tu departamento</option>
-            @foreach ($states as $state)
-                <option value="{{ $state->id }}">{{ $state->name }}</option>
-            @endforeach
-        </select>
+            <div>
+                <label for="sex" class="mb-2 text-sm font-medium text-white-900">Departamentos</label>
+                <select id="sexSign" x-model="state"
+                    class="bg-white-50 border border-gray-300 text-white-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5"
+                    required>
+                    <option value="" selected disabled>Selecciona tu departamento</option>
+                    @foreach ($states as $state)
+                        <option value="{{ $state->id }}">{{ $state->name }}</option>
+                    @endforeach
+                </select>
+            </div>
+
+            <div x-show="state != ''">
+                <label for="sex" class="mb-2 text-sm font-medium text-white-900">Municipios</label>
+                <select id="sexSign" x-model="town"
+                    class="bg-white-50 border border-gray-300 text-white-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5"
+                    required>
+                    <option value="" selected disabled>Selecciona tu municipio</option>
+                    @foreach ($towns as $town)
+                        <option value="{{ $town->id }}" :class="{{ $town->state_id }} == state ? '' : 'hidden'">
+                            {{ $town->name }}</option>
+                    @endforeach
+                </select>
+            </div>
+
+            <div x-show="town != ''">
+                <label for="sex" class="block mb-2 text-sm font-medium text-white-900">Distritos</label>
+                <select id="sexSign" x-model="district"
+                    class="bg-white-50 border border-gray-300 text-white-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5"
+                    required>
+                    <option value="" selected disabled>Selecciona tu sexo</option>
+                    @foreach ($districts as $district)
+                        <option value="{{ $district->id }}" :class="{{ $district->town_id }} == town ? '' : 'hidden'">
+                            {{ $district->name }}</option>
+                    @endforeach
+                </select>
+            </div>
+        </form>
     </div>
-
-    <div x-show="state != ''">
-        <label for="sex" class="block mb-2 text-sm font-medium text-white-900">Municipios</label>
-        <select id="sexSign" x-model="town" class="bg-white-50 border border-gray-300 text-white-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:focus:ring-blue-500 dark:focus:border-blue-500" required>
-            <option value="" selected disabled>Selecciona tu municipio</option>
-            @foreach ($towns as $town)
-                <option value="{{ $town->id }}" :class="{{ $town->state_id }} == state ? '' : 'hidden'">{{ $town->name }}</option>
-            @endforeach
-        </select>
-    </div>
-
-    <div x-show="town != ''">
-        <label for="sex" class="block mb-2 text-sm font-medium text-white-900">Distritos</label>
-        <select id="sexSign" x-model="district" class="bg-white-50 border border-gray-300 text-white-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:focus:ring-blue-500 dark:focus:border-blue-500" required>
-            <option value="" selected disabled>Selecciona tu sexo</option>
-            @foreach ($districts as $district)
-                <option value="{{ $district->id }}" :class="{{ $district->town_id }} == town ? '' : 'hidden'">{{ $district->name }}</option>
-            @endforeach
-        </select>
-    </div>
-
-
-</form>
 </x-layout>
--- storage/framework/views/3ab651ff6bb2b6c7a6da26b88a8eab4a.php
+++ storage/framework/views/3ab651ff6bb2b6c7a6da26b88a8eab4a.php
@@ -15,7 +15,7 @@
 <div>
     <label for="last_name" class="block mb-2 text-sm font-medium text-black-900"><?php echo e($label); ?></label>
     <input type="<?php echo e($type); ?>" name="<?php echo e($name); ?>"
-        class="bg-gray-50 border border-gray-300 text-black-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:focus:ring-blue-500 dark:focus:border-blue-500 <?php echo e($class); ?>"
+        class="bg-gray-50 border border-gray-300 text-black text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 <?php echo e($class); ?>"
         placeholder="<?php echo e($placeholder); ?>" <?php if($required): echo 'required'; endif; ?>>
 </div>
 
