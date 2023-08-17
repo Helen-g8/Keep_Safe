2c8d4d86e53f7815a88b91efadcb278f82328b2e
alexticas2006 <137197550+alexticas2006@users.noreply.github.com>
2023-08-17 08:23:26 -0600

    .

--- storage/framework/views/9465b80c0a0ab3e2048eb21aec3e8f7f.php
+++ storage/framework/views/9465b80c0a0ab3e2048eb21aec3e8f7f.php
@@ -41,6 +41,11 @@
 <?php $component = $__componentOriginal71c6471fa76ce19017edc287b6f4508c; ?>
 <?php unset($__componentOriginal71c6471fa76ce19017edc287b6f4508c); ?>
 <?php endif; ?>
+            <div class="grid justify-items-center">
+                <button type="submit"
+                        class="mt-4 text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Submit</button>
+            </div>
+
         </form>
     </div>
  <?php echo $__env->renderComponent(); ?>
@@ -49,5 +54,4 @@
 <?php $component = $__componentOriginal71c6471fa76ce19017edc287b6f4508c; ?>
 <?php unset($__componentOriginal71c6471fa76ce19017edc287b6f4508c); ?>
 <?php endif; ?>
-
 <?php /**PATH /home/alexticas2006/GitHub/Keep_Safe/resources/views/signUp.blade.php ENDPATH**/ ?>
