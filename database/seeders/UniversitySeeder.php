195981e2f0505e171f2d446f21b488c4ddd6d027
Hernandez2506 <137920074+Hernandez2506@users.noreply.github.com>
2023-08-18 09:44:23 -0600

    g

--- database/seeders/UniversitySeeder.php
+++ database/seeders/UniversitySeeder.php
@@ -16,60 +16,140 @@ class UniversitySeeder extends Seeder
     {
         $universities = [
             [
-                'name' => 'Universidad de El Salvador (UES) - Campus Ahuachapán',
-                'state' => 'Ahuachapán'
+                'district' => 'Ahuachapán',
+                'name' => [
+                    'Universidad de El Salvador (UES) - Campus Ahuachapán'
+                ]
             ],
+
             [
-                'name' => 'Universidad Francisco Gavidia (UFG) - Campus Sensuntepeque',
-                'state' => 'Cabañas'
+                'state' => 'Cabañas',
+                'name' => [
+                    'Universidad Francisco Gavidia (UFG) - Campus Sensuntepeque',
+                ]
             ],
+
             [
-                'name' => 'Universidad Nacional de El Salvador (UNSA) - Campus Chalatenango',
-                'state' => 'Chalatenango'
+                'state' => 'Chalatenango',
+                'name' => [
+                    'Universidad Nacional de El Salvador (UNSA) - Campus Chalatenango'
+                ]
             ],
+
             [
-                'Universidad Don Bosco (UDB) - Campus Cuscatlán',
-                'state' =
+                'state' => 'Cuscatlán',
+                'name' => [
+                    'Universidad Don Bosco (UDB) - Campus Cuscatlán'
+                ]
             ],
-            'Universidad de El Salvador (UES) - Campus Santa Tecla',
-            'Universidad Dr. José Matías Delgado (UMG) - Campus Santa Tecla',
-            'Universidad Francisco Gavidia (UFG) - Campus Santa Tecla',
-            'Universidad de El Salvador (UES) - Campus San Luis Talpa',
-            'Universidad Nacional de El Salvador (UNSA) - Campus Zacatecoluca',
-            'Universidad de El Salvador (UES) - Campus La Unión',
-            'Universidad de El Salvador (UES) - Campus San Francisco Gotera',
-            'Universidad de El Salvador (UES) - Campus San Miguel',
-            'Universidad Gerardo Barrios (UGB) - Campus San Miguel',
-            'Universidad de El Salvador (UES) - Campus San Salvador',
-            'Universidad Tecnológica de El Salvador (UTEC)',
-            'Universidad Dr. José Matías Delgado (UMG) - Campus San Salvador',
-            'Universidad Francisco Gavidia (UFG) - Campus San Salvador',
-            'Universidad Tecnológica (UTEC) - Campus San Salvador',
-            'Universidad de El Salvador (UES) - Campus San Vicente',
-            'Universidad de El Salvador (UES) - Campus Santa Ana',
-            'Universidad Católica de El Salvador (UNICAES) - Campus Santa Ana',
-            'Universidad Tecnológica de El Salvador (UTEC) - Campus Santa Ana',
-            'Universidad de El Salvador (UES) - Campus Sonsonate',
-            'Universidad Tecnológica de El Salvador (UTEC) - Campus Sonsonate',
-            'Universidad de El Salvador (UES) - Campus Usulután',
-            'Universidad Nueva San Salvador',
-            'Universidad Politécnica de el Salvador',
-            'Universidad Cristiana de las Asambleas de Dios',
-            'Universidad de Oriente',
-            'Universidad Autónoma de Santa Ana',
-            'Escuela Superior de Economia y Negocios (ESEN)',
-            'Universidad Arbert Einstein',
-            'Escuela de comunicación Mónica Herrera',
-            'Universidad Andrés Bello',
-            'INCAE Business School',
-            'Escuela de Aviación Aerotraining (San Salvador)',
+
+            [
+                'state' => 'La Libertad',
+                'name' => [
+                    'Universidad de El Salvador (UES) - Campus Santa Tecla',
+                    'Universidad Dr. José Matías Delgado (UMG) - Campus Santa Tecla',
+                    'Universidad Francisco Gavidia (UFG) - Campus Santa Tecla',
+                    'Escuela Superior de Economia y Negocios (ESEN)',
+                    'Universidad Albert Einstein',
+                    'Escuela de comunicación Mónica Herrera',
+                ]
+            ],
+
+            [
+                'state' => 'La Paz',
+                'name' => [
+                    'Universidad de El Salvador (UES) - Campus San Luis Talpa',
+                    'Universidad Nacional de El Salvador (UNSA) - Campus Zacatecoluca',
+                ],
+            ],
+
+            [
+                'state' => 'La Unión',
+                'name' => [
+                    'Universidad de El Salvador (UES) - Campus La Unión',
+                ]
+            ],
+
+            [
+                'state' => 'Morazán',
+                'name' => [
+                    'Universidad de El Salvador (UES) - Campus San Francisco Gotera',
+                ]
+            ],
+
+            [
+                'state' => 'San Salvador',
+                'name' => [
+                    'Universidad de El Salvador (UES) - Campus San Salvador',
+                    'Universidad Centroamericana "José Simeón Cañas" (UCA)',
+                    'Universidad Tecnológica de El Salvador (UTEC)',
+                    'Universidad Dr. José Matías Delgado (UMG) - Campus San Salvador',
+                    'Universidad Francisco Gavidia (UFG) - Campus San Salvador',
+                    'Universidad Tecnológica (UTEC) - Campus San Salvador', 
+                    'Escuela de Aviación Aerotraining', 
+                    'Universidad Nueva San Salvador',   
+                    'Universidad Cristiana de las Asambleas de Dios',
+                    'Universidad Politécnica de El Salvador',
+                    'Universidad Andrés Bello',
+                    'Universidad Andrés Bello - Campus 2',
+                    'INCAE Business School',
+                ]
+            ],
+
+            [
+                'state' => 'San Miguel',
+                'name' => [
+                    'Universidad de El Salvador (UES) - Campus San Miguel',
+                    'Universidad Gerardo Barrios (UGB) - Campus San Miguel',
+                    'Universidad de Oriente',
+                ]
+                ],
+
+            [
+                'state' => 'San Vicente',
+                'name' => [
+                    'Universidad de El Salvador (UES) - Campus San Vicente',
+                ]
+            ],
+
+            [
+                'state' => 'Santa Ana',
+                'name' => [
+                    'Universidad de El Salvador (UES) - Campus Santa Ana',
+                    'Universidad Católica de El Salvador (UNICAES) - Campus Santa Ana',
+                    'Universidad Tecnológica de El Salvador (UTEC) - Campus Santa Ana', 
+                    'Universidad Autónoma de Santa Ana',       
+                ]
+            ],
+
+            [
+                'state' => 'Sonsonate',
+                'name' => [
+                    'Universidad de El Salvador (UES) - Campus Sonsonate',
+                    'Universidad Tecnológica de El Salvador (UTEC) - Campus Sonsonate',
+                ]
+            ],
+
+            [
+                'state' => 'Usulután',
+                'name' => [
+                    'Universidad de El Salvador (UES) - Campus Usulután',
+                ]
+            ],
+
 
         ];
 
-        University::factory(count($universities))->sequence(fn($sqn) => [
-            'name' => $universities[$sqn->index]['name'],
-            'district_id' => District::where('name', $universities[$sqn->index]['distrito'])
-        ])->create();
+
+        foreach ($universities as $universitiesarray) {
+            $district = District::where('name', $universitiesarray['district'])->first()->id;
+            
+            $universitiesnames = $universitiesarray['name'];
+            University::factory(count($universitiesnames))->sequence(fn($sqn) => [
+                'name' => $universitiesnames[$sqn->index],
+                'district_id' => $district
+            ])->create();
+        }
 
 
     }
