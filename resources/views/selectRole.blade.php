53ca9bb25c566f51fe6905c6af5283b2a6a1b164
Helen-g8 <136003068+Helen-g8@users.noreply.github.com>
2023-08-22 09:43:12 -0600

    color

--- resources/views/selectRole.blade.php
+++ resources/views/selectRole.blade.php
@@ -3,11 +3,11 @@
             <label class="text-center block mb-20 text-xl font-bold">Selecciona tu rol</label>
             <div class="grid gap-6 md:grid-cols-2 mb-10 mt-8 px-64 m-10">
                 <a href="/signUp?rol=1" type="button"
-                    class="text-white bg-[#D99748] from-cyan-400 via-cyan-500 to-cyan-600 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-cyan-300 font-medium rounded-lg text-sm px-2 py-10 text-center mr-6 mb-2">Arrendador</a>
+                    class="text-white bg-[#3d2A58] from-cyan-400 via-cyan-500 to-cyan-600 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-cyan-300 font-medium rounded-lg text-sm px-2 py-10 text-center mr-6 mb-2">Arrendador</a>
 
                 <a href="/signUp?rol=2" type="button"
 
-                    class="text-white bg-[#D99748] from-cyan-400 via-cyan-500 to-cyan-600 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-cyan-300 font-medium rounded-lg text-sm px-2 py-10 text-center mr-4 mb-2 ml-6">Arrendatario</a>
+                    class="text-white bg-[#3d2A58] from-cyan-400 via-cyan-500 to-cyan-600 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-cyan-300 font-medium rounded-lg text-sm px-2 py-10 text-center mr-4 mb-2 ml-6">Arrendatario</a>
             </div>
         </div>
 </x-layout>
