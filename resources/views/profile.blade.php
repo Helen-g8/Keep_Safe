c6a446b3ddc0b4e9d4f9f176ff61424c424b4970
Helen-g8 <136003068+Helen-g8@users.noreply.github.com>
2023-08-25 17:54:17 -0600

    profile

--- resources/views/profile.blade.php
+++ resources/views/profile.blade.php
@@ -3,21 +3,35 @@
 
 
 
-    <div class="bg-orange-300 min-h-screen grid grid-cols-2 gap-4 items-center justify-center">
+    <div class="bg-orange-300 min-h-screen grid grid-cols-1 gap-4 items-center ">
 
-        <div class="flex justify-around items-center mb-4">
+        <div class="flex justify-around items-center ">
             <img src="{{ asset('profile.jpg') }}" alt="arrendatario" width="300" height="400"
             class="">
 
-            <div class="list-none">
+
+
+            <div class="">
+                <label class="text-lg font-bold">Name</label>
+                <p class="text-xl">{{$user->first_name}}</p>
+            </div>
+
+            <div class="">
+                <label class="text-lg font-bold">Number</label>
+                <p class="text-xl">{{$user->phone}}</p>
+            </div>
+            <div class="">
+
                 <div class="mb-4">
                     <label class="text-lg font-bold">Rol</label>
                     <p class="text-xl">{{$user->role_id}}</p>
                 </div>
+
                 <div class="mb-4">
-                    <label class="text-lg font-bold">Edad</label>
+                    <label class="text-lg font-bold"></label>
                     <p class="text-xl">{{$user->age}}</p>
                 </div>
+
 
 
             </div>
