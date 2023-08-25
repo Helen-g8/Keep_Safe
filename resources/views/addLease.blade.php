b405679f3f61d03059510fc1676f991a21785358
Helen-g8 <136003068+Helen-g8@users.noreply.github.com>
2023-08-25 08:40:22 -0600

    go

--- resources/views/addLease.blade.php
+++ resources/views/addLease.blade.php
@@ -69,8 +69,10 @@
                 </select>
             </div>
             <br>
-            <button type="submit"
-                class="w-96 bg-blue-600 hover:bg-blue-700 focus:ring-4 focus:outline-none focus:ring-blue-300 text-white font-medium rounded-lg text-lg py-2.5 transition-all duration-300 transform hover:scale-105 focus:ring focus:ring-opacity-50">Add</button>
+            <x-button>
+                
+            </x-button>
+
         </form>
     </div>
 </x-layout>
