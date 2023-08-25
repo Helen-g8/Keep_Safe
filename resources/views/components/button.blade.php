6d90a07e4194ba23a16988e0bdae298d518f14e3
alexticas2006 <137197550+alexticas2006@users.noreply.github.com>
2023-08-25 09:56:16 -0600

    rental info

--- resources/views/components/button.blade.php
+++ resources/views/components/button.blade.php
@@ -1,6 +1,6 @@
 @props(['label', 'type' => 'button', 'color' => '#BF7534', 'class' => ''])
 
 <button type="{{ $type }}"
-    class="text-white hover:bg-blue-800 bg-[{{ $color }}] focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-6 py-2 text-center {{ $class }}">
+    class="text-white hover:bg-purple-900 bg-[{{ $color }}] focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-6 py-2 text-center {{ $class }}">
     {{ $label }}
 </button>
