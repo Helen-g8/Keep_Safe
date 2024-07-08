fe02e01f199155c9a977ba7f51e705c50e204bdb
alexito25uwu <137197550+alexticas2006@users.noreply.github.com>
2024-07-08 10:12:56 -0600

    a

--- resources/views/Tchat.blade.php
+++ resources/views/Tchat.blade.php
@@ -11,7 +11,7 @@
             </div>
             <!-- Input box -->
             <form id="messageForm" class="bg-gray-100 px-4 py-3 border-t border-gray-300 flex items-center">
-                <input id="messageInput" type="text" 
+                <input id="message_input" type="text" 
                     class="w-full px-3 py-2 mr-2 border border-gray-300 rounded-lg focus:outline-none focus:border-blue-500"
                     placeholder="Type your message..." required>
                 <button type="submit"
