9bb1f30266ba9f75c126a74aee7578ed78ed0fcc
alexito25uwu <137197550+alexticas2006@users.noreply.github.com>
2024-07-15 10:00:02 -0600

    APPEREANCE

--- resources/views/arrendamiento.blade.php
+++ resources/views/arrendamiento.blade.php
@@ -1,12 +1,8 @@
 <x-layout>
-
-    <!-- Primera mitad (parte izquierda) -->
-    <div class="flex items-center justify-center mt-6">
+    <div class="flex items-center justify-around mt-6">
         <div class="text-center">
             <img src="{{ asset('casa_6.jpg') }}" alt="Lease photo" width="300" height="300"
                 class="mx-auto my-4 rounded-lg shadow-xl">
-
-
 
             <p class="text-2xl font-extrabold text-[#BF7534]">
                 Lease at: <span class="ml-1 text-black font-semibold font-palatino">{{ $arrendamiento->district->name }},
@@ -14,14 +10,11 @@
             </p>
         </div>
     </div>
-    </div>
 
-    <!-- Segunda mitad (parte derecha) -->
-    <div class="flex justify-around mt-[-280px]">
+    <div class="flex justify-center mt-4">
         <div class="text-left shadow-xl rounded-[10px] p-8">
             <!-- Sección 1: General lease information -->
-            <label class="underline text-lg font-extrabold">General lease
-                information</label>
+            <label class="mb-8 text-[#4B2579] text-lg font-extrabold">General lease information</label><br><br>
 
             <p class="text-lg font-bold">
                 Rooms: <span class="text-[#BF7534]">{{ $arrendamiento->rooms }}</span>
@@ -44,9 +37,9 @@
             </p>
         </div>
 
-        <div class="text-left shadow-xl rounded-[10px] p-8">
+        <div class="text-left shadow-xl rounded-[10px] p-8 mx-[40px]">
             <!-- Sección 2: Lease conditions -->
-            <label class="mt-4 underline text-lg font-extrabold">Lease conditions</label>
+            <label class="mb-8 text-[#4B2579] text-lg font-extrabold">Lease conditions</label><br><br>
             @foreach ($arrendamiento->conditions as $condition)
                 <p class="text-lg font-bold text-left">
                     {{ $condition->name }}: <span
@@ -57,7 +50,7 @@
 
         <div class="text-left shadow-xl rounded-[10px] px-[20px] py-[10px]">
             <!-- Sección 3: Lease services -->
-            <label class="mt-4 underline text-lg font-extrabold">Lease services</label>
+            <label class="mb-8 text-[#4B2579] text-lg font-extrabold">Lease services</label><br><br>
             @foreach ($arrendamiento->services as $service)
                 <p class="text-lg font-bold text-left">
                     {{ $service->name }}: <span
@@ -67,7 +60,7 @@
         </div>
     </div>
 
-    <div class="text-center mt-10 mb-8">
+    <div class="text-center mt-6 mb-[20px]">
         <!-- Botón Get in touch with the owner -->
         @if ($arrendamiento->rooms > 0)
             <a href="/Tchat">
@@ -81,5 +74,4 @@
             <x-button class="shadow-xl text-xl bg-[#4B2579] ml-[25px]" label='See reviews' />
         </a>
     </div>
-
 </x-layout>
